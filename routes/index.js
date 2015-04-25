2993254ad1b8c9ecedcd433f5b9f8a702f782767
yhung119 <yhung119@illinois.edu>
2015-04-25 02:55:12 -0500

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var bootstrap = require('boostrap')
+<<<<<<< HEAD
+=======
+var bootstrap = require('bootstrap');
+var shortid = require('shortid');
+
+>>>>>>> origin/master
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Market Making' });
@@ -10,8 +15,17 @@ router.get('/rooms', function (req, res, next) {
 	res.render('rooms', {title: 'Rooms'});
 });
 
+router.get('/room/create', function (req, res, next) {
+	// other settings
+	var newID = shortid.generate();
+	res.render('newroom', {title: 'Create New Room', newID: newID});
+})
+
 router.get('/rooms/:roomid', function (req, res, next) {
-	res.render('game', {title: 'Game'});
+	res.render('game', {title: 'Game', roomid: res.params.roomid});
+});
+router.get('/contact', function (req, res, next){
+	res.render('contact', {title: 'Contact'});
 });
 
 module.exports = router;
