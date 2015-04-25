08b0cc9ad7e61d2175f0a564efc7c9ee4f1c650d
Andrew Yang <ayang14@illinois.edu>
2015-04-25 01:54:56 -0500

    More index paths

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var bootstrap = require('boostrap')
+var bootstrap = require('bootstrap');
+var shortid = require('shortid');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Market Making' });
@@ -10,8 +12,14 @@ router.get('/rooms', function (req, res, next) {
 	res.render('rooms', {title: 'Rooms'});
 });
 
+router.get('/room/create', function (req, res, next) {
+	// other settings
+	var newID = shortid.generate();
+	res.render('newroom', {title: 'Create New Room', newID: newID});
+})
+
 router.get('/rooms/:roomid', function (req, res, next) {
-	res.render('game', {title: 'Game'});
+	res.render('game', {title: 'Game', roomid: res.params.roomid});
 });
 
 module.exports = router;
