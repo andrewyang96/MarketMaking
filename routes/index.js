dbf3e499f005f6d3fbad601dbda6f1348b66f3b4
Andrew Yang <ayang14@illinois.edu>
2015-04-25 01:30:30 -0500

    Added paths

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Market Making' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Market Making' });
+});
+
+router.get('/rooms', function (req, res, next) {
+	res.render('rooms', {title: 'Rooms'});
+});
+
+router.get('/rooms/:roomid', function (req, res, next) {
+	res.render('game', {title: 'Game'});
 });
 
 module.exports = router;
