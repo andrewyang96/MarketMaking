9d1842d5146a2e0e8bc7af5649b9b5515b2880e7
Andrew Yang <ayang14@illinois.edu>
2015-04-25 03:15:13 -0500

    Create redirect

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var shortid = require('shortid');
+var firebase = require('firebase');
+var ref = new Firebase("https://market-making.firebaseio.com/");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -13,9 +15,24 @@ router.get('/rooms', function (req, res, next) {
 });
 
 router.get('/room/create', function (req, res, next) {
-	// other settings
-	var newID = shortid.generate();
-	res.render('newroom', {title: 'Create New Room', newID: newID});
+	var roomName = req.body.name;
+	var roomType = req.body.type;
+	var numRounds = req.body.numrounds;
+	var roundLength = req.body.roundlength;
+	if (roomName && roomType && numRounds && roundLength) {
+		var newID = shortid.generate();
+		ref.child("rooms").child(newID).set({
+			roomName: roomName,
+			roomType: roomType,
+			numRounds: numRounds,
+			roundLength: roundLength,
+			startTime: null
+		}, function () {
+			res.redirect("/rooms/" + newID);
+		});
+	} else {
+		res.render('newroom', {title: 'Create New Room'});
+	}
 })
 
 router.get('/rooms/:roomid', function (req, res, next) {
