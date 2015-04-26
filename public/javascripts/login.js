0a56dadb5f81fe31135ff990ea435eee238f7ac8
yhung119 <yhung119@illinois.edu>
2015-04-26 03:32:45 -0500

    table

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -85,6 +85,42 @@ function anythingElse() {
 	var roomSrc = $('#rooms').html(); // indicator for rooms
 	var gameSrc = $('#game-area').html(); // indicator for waiting list
 	var tradeSrc = $('#trade-area').html(); // indicator for trading game
+	var time = $('#time').html();
+	if(time){
+		function startTimer(duration, display) {
+    	var start = Date.now(),
+        diff,
+        seconds;
+    	function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        
+        seconds = (diff % 60) | 0;
+
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        // display.textContent = seconds; 
+
+	        if (diff <= 0) {
+	            // add one second so that the count down starts at the full duration
+	            // example 05:00 not 04:59
+	            start = Date.now() + 200;
+	        }
+		    };
+		    // we don't want to wait a full second before the timer starts
+		    timer();
+			    setInterval(timer, 200);
+			}
+
+			window.onload = function () {
+			    var fiveMinutes = 60,
+			        display = document.querySelector('#time');
+			    startTimer(fiveMinutes, display);
+			};
+		}
 	if (roomSrc) {
 		// initialize handlebars variables
 		var roomTemplate = Handlebars.compile(roomSrc);
